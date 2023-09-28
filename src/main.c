b8c453c950b84925c130b00de5470e6199f9ca8b
Julian Martus <martus@is.mpg.de>
2023-09-28 09:47:23 +0200

    Added ESp-Now broadcast registering

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #include "esp_now.h"
 #include "esp_private/wifi.h"
 #include "esp_private/esp_wifi_types_private.h"
+#include "driver/gptimer.h"
 
 #include "driver/uart.h"
 #include "driver/gpio.h"
@@ -21,9 +22,6 @@
 #include "hal/usb_serial_jtag_ll.h"
 #include "driver/usb_serial_jtag.h"
 
-#define CONFIG_ESPNOW_CHANNEL 1
-esp_now_peer_info_t *peer;
-
 // Define the structure for the data to be received over ESP-NOW
 #define ADC_BUFFER_LEN (ESP_NOW_MAX_DATA_LEN)
 #define BUFFER_SCALE 8
@@ -35,12 +33,19 @@ uint8_t buffer_espnow_rx[NUM_BUFFER][ADC_BUFFER_LEN + ESP_NOW_ETH_ALEN];
 #define USB_MESSAGE_LEN 16
 uint8_t buffer_usb_rx[USB_MESSAGE_LEN];
 // a0:76:4e:44:51:38
-static uint8_t sensor_mac2[ESP_NOW_ETH_ALEN] = {0xA0, 0x76, 0x4E, 0x44, 0x51, 0x38};
-// a0:76:4e:41:bd:dc
-static uint8_t sensor_mac1[ESP_NOW_ETH_ALEN] = {0xA0, 0x76, 0x4E, 0x41, 0xBD, 0xDC};
-// a0:76:4e:41:e6:70
-static uint8_t sensor_mac[ESP_NOW_ETH_ALEN] = {0xA0, 0x76, 0x4E, 0x41, 0xE6, 0x70};
+
+// static uint8_t sensor_mac2[ESP_NOW_ETH_ALEN] = {0xA0, 0x76, 0x4E, 0x44, 0x51, 0x38};
+//  a0:76:4e:41:bd:dc
+// static uint8_t sensor_mac1[ESP_NOW_ETH_ALEN] = {0xA0, 0x76, 0x4E, 0x41, 0xBD, 0xDC};
+//  a0:76:4e:41:e6:70
+// static uint8_t sensor_mac[ESP_NOW_ETH_ALEN] = {0xA0, 0x76, 0x4E, 0x41, 0xE6, 0x70};
+
+#define CONFIG_ESPNOW_CHANNEL 1
+uint8_t mac_address[ESP_NOW_ETH_ALEN];
+esp_now_peer_info_t *peer_broadcast;
 static uint8_t broadcast_mac[ESP_NOW_ETH_ALEN] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+static const uint8_t espcommand_channel[8] = {0x74, 0x79, 0x73, 0x80, 0xec, 0x3c, 0xce, 0x13};
 
 #define UART_PORT UART_NUM_0
 #define UART_TX_GPIO_NUM UART_PIN_NO_CHANGE
@@ -60,21 +65,22 @@ void usb_task(void *pvParameters)
 
     memset(buffer_espnow_rx, 0x55, sizeof(buffer_espnow_rx) * sizeof(uint8_t));
     ESP_LOGI("USB", "USB initialised");
-    uint8_t cnt = 0;
+
     while (true)
     {
         if (send)
         {
             send--;
 
-            // usb_serial_jtag_write_bytes(header, 4, 1 / portTICK_PERIOD_MS);
+            usb_serial_jtag_write_bytes(header, 4, portMAX_DELAY);
             usb_serial_jtag_write_bytes(&buffer_espnow_rx[send][0], ADC_BUFFER_LEN + ESP_NOW_ETH_ALEN, portMAX_DELAY);
         }
         else if (usb_serial_jtag_read_bytes(buffer_usb_rx, USB_MESSAGE_LEN, pdMS_TO_TICKS(1)))
         {
             // len = usb_serial_jtag_ll_read_rxfifo(buffer1, ADC_BUFFER_LEN * sizeof(uint8_t));
+            ESP_LOGI("ESP-NOW", "Received USB-command. Starting measurement");
             REG_WRITE(GPIO_OUT_W1TS_REG, BIT2); // LOW
-            esp_err_t ret = esp_now_send(peer->peer_addr, buffer_usb_rx, 1);
+            esp_err_t ret = esp_now_send(peer_broadcast->peer_addr, buffer_usb_rx, 1);
             if (ret != ESP_OK)
             {
                 ESP_LOGE("ESP-NOW", "Error sending data: %s", esp_err_to_name(ret));
@@ -112,6 +118,29 @@ void espnow_recv_cb(const esp_now_recv_info_t *esp_now_info, const uint8_t *data
 static void espnow_send_cb(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
 }
+
+void esp_now_channel_broadcast(void *pvParameters)
+{
+    while (1)
+    {
+
+        vTaskDelay(pdMS_TO_TICKS(5000));
+        if (esp_now_is_peer_exist(peer_broadcast->peer_addr))
+        { // double-check
+            ESP_LOGI("ESP-NOW", "Broadcasting channel for registration\nChannel %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x",
+                     espcommand_channel[0],
+                     espcommand_channel[1],
+                     espcommand_channel[2],
+                     espcommand_channel[3],
+                     espcommand_channel[4],
+                     espcommand_channel[5],
+                     espcommand_channel[6],
+                     espcommand_channel[7]);
+            ESP_ERROR_CHECK(esp_now_send(peer_broadcast->peer_addr, espcommand_channel, sizeof(espcommand_channel)));
+        }
+    }
+}
+
 void app_main()
 {
     esp_err_t ret;
@@ -146,21 +175,23 @@ void app_main()
     ESP_ERROR_CHECK(esp_now_register_recv_cb(espnow_recv_cb));
 
     /* Add Dongle information to peer list. */
-    peer = malloc(sizeof(esp_now_peer_info_t));
+    peer_broadcast = malloc(sizeof(esp_now_peer_info_t));
 
-    memset(peer, 0, sizeof(esp_now_peer_info_t));
-    peer->channel = CONFIG_ESPNOW_CHANNEL;
-    peer->ifidx = ESP_IF_WIFI_STA;
-    peer->encrypt = false;
-    memcpy(peer->peer_addr, broadcast_mac, ESP_NOW_ETH_ALEN);
-    ESP_ERROR_CHECK(esp_now_add_peer(peer));
+    memset(peer_broadcast, 0, sizeof(esp_now_peer_info_t));
+    peer_broadcast->channel = CONFIG_ESPNOW_CHANNEL;
+    peer_broadcast->ifidx = ESP_IF_WIFI_STA;
+    peer_broadcast->encrypt = false;
+    memcpy(peer_broadcast->peer_addr, broadcast_mac, ESP_NOW_ETH_ALEN);
+    ESP_ERROR_CHECK(esp_now_add_peer(peer_broadcast));
 
-    uint8_t mac_address[6];
     esp_wifi_get_mac(ESP_IF_WIFI_STA, mac_address);
-    ESP_LOGI("IDLE", "%02x:%02x:%02x:%02x:%02x:%02x\n",
+    ESP_LOGI("ESP-NOW", "ESP-Now initialised. MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
              mac_address[0], mac_address[1], mac_address[2],
              mac_address[3], mac_address[4], mac_address[5]);
+    ESP_LOGI("USB", "USB-task started");
     xTaskCreate(usb_task, "usb_task", configMINIMAL_STACK_SIZE * 4, NULL, tskIDLE_PRIORITY, NULL);
+
+    xTaskCreate(esp_now_channel_broadcast, "esp_now_channel_broadcast", configMINIMAL_STACK_SIZE * 4, NULL, tskIDLE_PRIORITY, NULL);
 
     while (0)
     {
